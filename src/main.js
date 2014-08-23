a974981427a6260113ed8a63f2fdf21b9cb0eca4
boboman13 <rich@safe-node.com>
2014-08-22 20:05:53 -0700

    Refractored code. Begin removal of ExpressJS...

--- src/main.js
+++ src/main.js
@@ -11,48 +11,11 @@ var app = express(),
 // Config stuff
 config.options.cache.expireTime = 1000 * 60 * config.options.cache.minutes_until_expire;
 
-// /r for resource
-//   http://placekitten.com/200/300
-//   /r/aHR0cDovL3BsYWNla2l0dGVuLmNvbS9nLzIwMC8zMDA=
-app.get('/r/:url', function(req, res) {
-	var encoded_url = req.params['url'],
-		buffer = new Buffer(encoded_url, 'base64'),
-		url = buffer.toString('utf8');
+function serveFromCache(filename, res) {
+	fs.createReadStream(filename).pipe(res.status(200));
+}
 
-	// check that URL at the door
-	if (config.resources.whitelist) {
-		var parsed = urlParser.parse(url);
-
-		if (config.resources.allowed.hosts.indexOf(parsed.hostname) == -1) {
-			res.status(500).send('Not allowed to access resource.');
-			return;
-		}
-	}
-
-	// cache related variables
-	var refreshCache = true,
-		writeStream = null,
-		filename = config.options.cache.directory + encoded_url;
-
-	// handle the cache
-	if (config.options.cache.use) {
-		if (fs.existsSync(filename)) {
-			var stats = fs.statSync(filename),
-				today = new Date().getTime();
-
-			// if it is not older than the expiry time, serve from cache
-			if (new Date(stats.mtime).getTime() + config.options.cache.expireTime > today) {
-				fs.createReadStream(filename).pipe(res.status(200));
-				return;
-			}
-		}
-	} else {
-		refreshCache = false;
-	}
-
-	if (refreshCache) writeStream = fs.createWriteStream(filename);
-
-	// go
+function serveFromExternal(url, res, writeStream) {
 	http.get(url, function(result) {
 		// check for 200 status code
 		if (result.statusCode !== 200) {
@@ -65,13 +28,53 @@ app.get('/r/:url', function(req, res) {
 
 		result.pipe(res);
 
-		if (refreshCache) result.pipe(writeStream);
+		if (typeof writeStream !== 'undefined') result.pipe(writeStream);
 
 	}).on('error', function() {
 		res.status(404).send('Resource not found.');
 	});
+}
+
+// /r for resource
+//   http://placekitten.com/200/300
+//   /r/aHR0cDovL3BsYWNla2l0dGVuLmNvbS9nLzIwMC8zMDA=
+app.get('/r/:url', function(req, res) {
+	var encoded_url = req.params['url'],
+		url = new Buffer(encoded_url, 'base64').toString('utf8');
+
+	// check that URL at the door
+	if (config.resources.whitelist) {
+		var parsed = urlParser.parse(url);
+
+		if (config.resources.allowed.hosts.indexOf(parsed.hostname) == -1) {
+			res.status(500).send('Not allowed to access resource.');
+			return;
+		}
+	}
+
+	// cache related variables
+	var filename = config.options.cache.directory + encoded_url;
+
+	// handle the cache
+	if (config.options.cache.use) {
+		fs.stat(filename, function(err, stats) {
+			if (!(typeof err !== 'undefined' && err == null)) {
+				serveFromExternal(url, res, fs.createWriteStream(filename));
+				return;
+			}
+
+			// check if it is older
+			if (new Date(stats.mtime).getTime() + config.options.cache.expireTime > new Date().getTime()) {
+				serveFromCache(filename, res);
+			} else {
+				serveFromExternal(url, res, fs.createWriteStream(filename));
+			}
+		});
+	} else {
+		serveFromExternal(url, res);
+	}
 });
 
-app.listen(process.env.development ? 3000 : config.server.port, config.server.host);
+app.listen(config.server.port, config.server.host);
 
 console.log('App started.');
